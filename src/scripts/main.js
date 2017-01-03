5755ae31044e287c9605d7b870668bdfe7801bc1
Jonathan Gruber <gruberjonathan@gmail.com>
2017-01-03 01:03:49 +0100

    Templating * Added JS for scroll links * Added FontAwesome icons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,35 @@
-console.log('\'Allo \'Allo!');
+let viewport = $('html, body');
+
+function clamp(n, min, max) {
+	return Math.min(Math.max(n, min), max);
+}
+
+function easeInOutQuart(t) {
+	return t < .5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
+}
+
+function scrollTo(top) {
+	// FIXME: subtract navbar height
+	viewport.animate({scrollTop: top}, 2000, easeInOutQuart);
+}
+
+$('.to-top').click(function () {
+	scrollTo(0);
+	return false;
+});
+
+$('nav a').click(function (e) {
+	e.preventDefault();
+
+	let to = $($(this).attr('href'));
+
+	if (!to.length)
+		return false;
+
+	let top = clamp(to.offset().top, 0, $(document).height());
+	scrollTo(top);
+});
+
+viewport.on('scroll mousedown wheel keydown touchstart', function () {
+	viewport.stop();
+});
