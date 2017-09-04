d0e967e47f458786ea2df590d47916db5757b3e7
Jonathan Gruber <gruberjonathan@gmail.com>
2017-09-04 02:29:26 +0200

    Removed unnecessary plugin

--- _plugins/env.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll
-  class EnvironmentVariablesGenerator < Generator
-    def generate(site)
-      site.config['env'] = ENV['JEKYLL_ENV'] || 'development'
-      # Add other environment variables to `site.config` here...
-    end
-  end
-end
