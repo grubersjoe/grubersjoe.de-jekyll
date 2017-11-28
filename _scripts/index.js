cee0e56a18f0a7e2fd9d05bed6fd0b3cf37c38d1
Jonathan Gruber <gruberjonathan@gmail.com>
2017-11-28 22:45:21 +0100

    Fixed linting errors

--- _scripts/index.js
+++ _scripts/index.js
@@ -1,27 +1,25 @@
-const $ = window.jQuery; // hosted on CDN for better performance
-
 $(() => {
   const viewport = $('html, body');
 
   function easeInOutCubic(t) {
-    return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+    // eslint-disable-next-line no-mixed-operators
+    return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
   }
 
-  function scrollTo(top, dur) {
-    dur = dur || 2000;
-    viewport.stop().animate({scrollTop: top}, dur, easeInOutCubic);
+  function scrollTo(top, dur = 2000) {
+    viewport.stop().animate({ scrollTop: top }, dur, easeInOutCubic);
   }
 
-  $('.to-top').click(function () {
+  $('.to-top').click(() => {
     scrollTo(0);
     return false;
   });
 
-  viewport.on('scroll mousedown wheel keydown touchstart', function () {
+  viewport.on('scroll mousedown wheel keydown touchstart', () => {
     viewport.stop();
   });
 
-  $('.collapsible').click(function () {
+  $('.collapsible').click(function handler() {
     $(this).toggleClass('open');
     $(this).find('.collapse').stop().slideToggle();
   });
@@ -29,19 +27,19 @@ $(() => {
 
 // TODO: try max-height approach
 // window.getHeight = function (elem) {
-// 	let style = getComputedStyle(elem);
+//   let style = getComputedStyle(elem);
 //
-// 	if (style.display !== 'none') {
-// 		return elem.clientHeight;
-// 	}
+//   if (style.display !== 'none') {
+//     return elem.clientHeight;
+//   }
 //
-// 	elem.style.display = 'block';
-// 	elem.style.visibility = 'hidden';
+//   elem.style.display = 'block';
+//   elem.style.visibility = 'hidden';
 //
-// 	let height = elem.clientHeight;
+//   let height = elem.clientHeight;
 //
-// 	elem.style.display = 'none';
-// 	elem.style.visibility = 'visible';
+//   elem.style.display = 'none';
+//   elem.style.visibility = 'visible';
 //
-// 	return height;
+//   return height;
 // };
